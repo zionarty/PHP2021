c4ae385fceedda87fc15966be76bd40e5b792416
maneerat.m <51904844+zionarty@users.noreply.github.com>
2021-03-15 22:21:48 +0700

    20210315

--- basic.php
+++ basic.php
@@ -119,5 +119,121 @@
             $x++;   #11
             ++$y;   #11
             ###
-            
+            $x =10; $y =20;
+            $y += ++$x;  # x =11 y=31
+            ###
+            $x =10; $y =20;
+            $y += $x++;  # x =11 y=30
+    #สำหรับเปรียบเทียบ(Comparison)
+        /*
+        ! หรือ not  
+            !(true) = false
+            !(false) = true
+        && หรือ and
+            true && true = true
+            true && false = false
+            false && false = false
+        || หรือ or
+            true || true = true
+            true || false = true
+            false || false = false
+        ^ หรือ xor
+            true ^ true = false
+            true ^ false = true
+            false ^ false = false
+        */
+        $a = (10 <= 9);     #false
+        $a = (10 == 10);    #true
+        $a = (10 == "10");  #true
+        $a = (10 === "10"); #false 10=int | "10"=string
+        $a = ("php" == "PHP");  #false ลักษณะตัวพิมพ์ต่างกัน
+    #สำหรับเปรียบเทียบทางตรรกะ
+        $a =!(1 == 2); #!(false) = true
+        $b = (1 == 2) && (1 > 0); #(false) && (true) = false
+        $c = (1 == 1) && (1 > 0); #(true) && (ture) = true
+        $d = (1 == 2) || (1 > 0); #(false) || (true) = true
+    #Ternary
+        $a = (1>0)? true : false; #true
+        $x =10;
+        $b = ($x%2 == 0)? "เลขคู่":"เลขคี่"; #เลขคู่;
+    #ลำดับความสำคัญของ operator
+        /*
+        1.()
+        2.++,--(วางไว้หน้าตัวแปร)
+        3.*,/,%
+        4.+,-
+        5.==,!=
+        6.&&
+        7.||
+        8.=,+=,-=,/=,*=
+        9.++,--(วางไว้หลังตัวแปร)
+        */
+        $a = 2*100/10; #(2*100)/10 = 20
+        $b = 100/10%2; #(100/10)%2 = 0
+        $c = 100%80*2;  #(100%80)*2 = 40
+        $d = 100/10+30; #(100/10)+30 =40
+        $e = 2*100/10+30; # (2*(100/10))+30 = 50
+        $x =5 ; $y = 10;
+        $z = $x++ * --$y; #5*(10-1) =45
+#ลักษณะเพิ่มเติม ชนิดข้อมูล string
+    #เทคนิคการเขียน string ยาวๆ
+        $str = "mr. somchai pie <br>";
+        $str .= "123/456 nonthaburi ";
+        $str .= "thailand";
+        echo $str;
+        ###
+        echo "mr. somchai pie <br>
+        123/456 nonthaburi
+        thailand
+        ";
+    #การแทรกค่าตัวแปรลงใน string (String Interpolation)
+        $name ="somchai";
+        $age = 30;
+        $career = "Programmer";
+        echo "My name is :$name age : $age career : $career";
+        ###
+        $w =100;
+        echo "width:{$w}px";
+    #เครื่องหมาย single quotes และ double quotes
+        /*
+        single quotes : ไม่สามารถแทรกค่าตัวแปรได้
+        double quotes : สามารถแทรกค่าตัวแปรได้
+        */
+        $name ="somchai";
+        $age = 30;
+        $career = "Programmer";
+        echo "My name is :$name age : $age career : $career";
+        echo 'My name is :$name age : $age career : $career';
+    #Escape Sequence
+        echo "You \"MUST NOT\" go here";
+        echo 'Don\'t remove';
+        $name = "somchai";
+        echo "The variable \$name store $name";
+        echo "We use backslash(\\) for escape sequence";
+#การใช้คำสั่ง if
+    #รูปแบบพื้นฐานของคำสั่ง if
+        /*
+        if(เงื่อนไข){
+            คำสั่งหากตรงเงื่อนไข
+        }
+        */
+        $x = 10 ; $y =10;
+        if($x == $y){
+            echo "$x เท่ากับ $y";
+        }
+        if($y != 0){
+            $z = $x/$y;
+        }
+        if(!defined('PRICE')){ #ถ้ายังไม่ได้กำหนดค่า PRICE
+            define('PRICE',299);
+        }
+        if(isset($x)){ #ถ้ากำหนดตัวแปร $x
+            echo $x;
+        }
+    #การใช้คำสั่ง if...else
+        
+
+
+
+
 ?>
